dd4dfa513a63286f5116dcc7bc61506199ec7281
filipJakab <flipovec321@gmail.com>
2016-10-09 21:38:42 +0200

    Update Program.cs
    another version. almost same result but defferent way

--- JakabFilip/ClassRoom_HW/ClassRoom_HW/Program.cs
+++ JakabFilip/ClassRoom_HW/ClassRoom_HW/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,31 +6,39 @@ using System.Threading.Tasks;
 
 namespace ClassRoom_HW
 {
-    class Program
+	class Program
     {
         static void Main(string[] args)
         {
-			List<Teacher> tchrs = new List<Teacher>(5);
-			List<Student> students = new List<Student>(30);
+			// List prepared for 6 classes
+			List<ClassRoom> clssrms = new List<ClassRoom>(6);
 
-			tchrs.Add(new Teacher(45, 176, 20, Man.Gender.Male, "ICT", "C210", "I2C", "Igor Novak", 0));
-			for (int i = 0; i < 30; i++)
+			clssrms.Add(new ClassRoom("I1C"));
+			clssrms.Add(new ClassRoom("E1C"));
+			clssrms.Add(new ClassRoom("I2C"));
+			clssrms.Add(new ClassRoom("E2C"));
+
+			// init classroom
+			clssrms[0].GenerateEntities("Igor Hnizdo", 15); // I1C
+			clssrms[1].GenerateEntities("Ivan Skritek", 15); // E1C
+			clssrms[2].GenerateEntities("Adam Nedockal", 16); // I2C
+			clssrms[3].GenerateEntities("Leos Pospisil", 16); // E2C
+
+			// printing classrooms
+			foreach(ClassRoom CR in clssrms)
 			{
-				students.Add(new Student(i + 3, i + 160, Man.Gender.Male, "Adam Novak", "PRG", "PRG", "MATH", true, i + 1));
+				Console.WriteLine($"students in classroom -> { CR.ID }");
+				Console.WriteLine("ID \t Name \t\t Age \t ID \t Name \t\t Age");
+				Console.WriteLine("------------------------------------------------------------\t||");
+				for (int i = 0; i < 20; i++)
+				{
+					Console.Write($" { CR.students[i].ID }.\t { CR.students[i].Name },\t { CR.students[i].Age } \t");
+					Console.Write($" { CR.students[i + 10].ID}.\t { CR.students[i + 10].Name },\t { CR.students[i + 10].Age } \t|| \n");
+				}
+				Console.WriteLine($"with teacher { CR.tchr.Name }.\t\t\t\t\t||");
+				Console.WriteLine("-------------------------------------------------------\t\t||");
 			}
 
-			Console.WriteLine("Class has been generated.");
-			foreach(Student stdnt in students)
-			{
-				Console.WriteLine($"{ stdnt.ID } Name: { stdnt.Name }, Age: { stdnt.Age }, Fav. Sbj: { stdnt.FavSubject }.");
-			}
-
-			Console.WriteLine("\nwith this teachers: ");
-			foreach(Teacher tchr in tchrs)
-			{
-				Console.WriteLine($"{ tchr.ID } Name: { tchr.Name }, Age: { tchr.Age }.");
-			}
-			
 			Console.ReadKey();
         }
     }
@@ -38,164 +46,117 @@ namespace ClassRoom_HW
     public class Man
     {
         public int Age;
-        public int Height;
+		public int ID;
         public string Name;
         public enum Gender { Male, Female };
-    }
 
-    public class Teacher : Man
-    {
-        public int LessonsCount;
-		public int ID;
-        public string MainSubject;
-        public string Room;
-        public string MainClass;
-		public string TchingCR;
-        public bool Teaching;
+		public Gender Gndr;
+	}
 
-        public Gender Gndr;
+	public class ClassRoom
+	{
+		public string ID;
+		public List<Student> students;
+		public Teacher tchr;
 
-        public Teacher(int A, int H, int lssns, Gender G, string MainSub, string R, string MainC, string N, int D)
-        {
-            this.Age = A;
-            this.Height = H;
-            this.LessonsCount = lssns;
-            this.Gndr = G;
-            this.MainSubject = MainSub;
-            this.Room = R;
-            this.MainClass = MainC;
-            this.Name = N;
-			this.ID = D;
-
-            Teaching = false;
-        }
-
-        public void GoTeach(string ClassRoom)
-        {
-            if (Teaching == false)
-            {
-                Teaching = true;
-                Console.WriteLine($"Teacher { Name } has started teaching at { ClassRoom }.");
-				TchingCR = ClassRoom;
-            }
-            else
-            {
-                Console.WriteLine($"Teacher { Name } is currently teaching in { TchingCR }.");
-            }
-        }
-
-		public void EndTeaching()
+		public ClassRoom(string ID)
 		{
-			if (Teaching == true)
+			this.ID = ID;
+
+			students = new List<Student>(31);
+		}
+
+		public void GenerateEntities(string TchrName, int StdAge)
+		{
+			for (int i = 0; i < 30; i++)
 			{
-				Console.WriteLine($"Teacher { Name } has stopped lesson in { TchingCR }.");
-				TchingCR = null;
+				students.Add(new Student("Adam Novak", i + 1, StdAge, Man.Gender.Male, "PRG"));
+			}
+
+			tchr = new Teacher(TchrName, 0, 46, "Math", Man.Gender.Male);
+		}
+	}
+
+	public class Teacher : Man
+    {
+		public bool IsTeaching;
+		public string Subject;
+
+        public Teacher(string N, int D, int A, string Sbj, Gender G)
+        {
+			this.Name = N;
+			this.ID = D;
+			this.Age = A;
+			this.Subject = Sbj;
+			this.Gndr = G;
+
+			IsTeaching = false;
+        }
+		
+		public void StartLesson(string ClassRoom)
+		{
+			if (IsTeaching == false) // TODO: najit tridu "ClassRoom" ("I2C") a nastavit studenty AtLsson = true pokud mozno
+			{
+				IsTeaching = true;
+				Console.WriteLine($"Teacher { this.Name } has started Lesson({ this.Subject }) at \"{ ClassRoom }\". ");
 			}
 			else
 			{
-				Console.WriteLine($"Teacher { Name } isnt teaching at the moment.");
+				Console.WriteLine($"Teacher { this.Name }, is already teaching \"{ ClassRoom }\".");
 			}
+		}
+
+		public void SetHW(string ToClass) // TODO: zada ukol urcite tride
+		{
+
 		}
     }
 
     public class Student : Man
     {
-        public int HWToDo;
-		public int ID;
-        public string FavSubject;
-        public string BadSubject;
-        public string Hobby;
-        public bool AtSchool;
+		public int Absent;
 		public bool Snack;
-		public bool InLesson;
+		public bool AtLesson;
+		public bool HasHW;
+		public string FavSub;
 
-        public Gender Gndr;
-
-        public Student(int A, int H, Gender G, string N, string Hbb, string FS, string BS, bool S, int D)
+        public Student(string N, int D, int A, Gender G, string FS)
         {
-            this.Age = A;
-            this.Height = H;
-            this.Gndr = G;
-            this.Name = N;
-            this.Hobby = Hbb;
-            this.FavSubject = FS;
-            this.BadSubject = BS;
-			this.Snack = S;
+			this.Name = N;
 			this.ID = D;
+			this.Age = A;
+			this.Gndr = G;
+			this.FavSub = FS;
 
-            AtSchool = false;
-			InLesson = false;
-            HWToDo = 0;
+			Absent = 0;
+			Snack = true;
+			AtLesson = false;
+			HasHW = false;
         }
 
-        public void GoToSchool()
-        {
-            if (AtSchool == false)
-            {
-                Console.WriteLine($"Student { Name } is going to school.");
-            }
-            else
-            {
-                Console.WriteLine($"Student { Name } is already at school.");
-            }
-        }
-
-        public void DoHW(int Amount)
-        {
-            if (HWToDo >= Amount)
-            {
-                Console.WriteLine($"Student { Name } has started ...");
-                for (int i = 0; i < Amount; i++)
-                {
-                    System.Threading.Thread.Sleep(500);
-                    Console.WriteLine($"{ i }. Student { Name } has done one HW.");
-                }
-
-		HWToDo -= Amount;
-                Console.WriteLine($"Student { Name } has done requested amount({ Amount }) of HW");
-            }
-            else
-            {
-                Console.WriteLine($"Student { Name } nema tolik HW, udela tedy vsechny({ HWToDo }).");
-            }
-        }
-
-		public void EatSnack()
+		public void JoinLesson(string Lssn, string Sbj)
 		{
-			if (Snack == true)
+			if (AtLesson == false)
 			{
-				Console.WriteLine($"Student { Name } is eating snack.");
-				Snack = false;
+				AtLesson = true;
+				Console.WriteLine($"Student { this.Name } has joinded lesson({ Sbj })");
 			}
 			else
 			{
-				Console.WriteLine($"Student { Name } dont have any snack.");
-			}
-		}
-
-		public void JoinLesson()
-		{
-			if (InLesson == false)
-			{
-				Console.WriteLine($"Student { Name } has joined lesson.");
-				InLesson = true;
-			}
-			else
-			{
-				Console.WriteLine($"Student { Name } is currently in Lesson.");
+				Console.WriteLine($"Student { this.Name } is already in Lesson({ Sbj }).");
 			}
 		}
 
 		public void LeaveLesson()
 		{
-			if (InLesson == true)
+			if (AtLesson == true)
 			{
-				Console.WriteLine($"Student { Name } has left lesson.");
-				InLesson = false;
+				AtLesson = false;
+				Console.WriteLine($"Student { this.Name } has left lesson.");
 			}
 			else
 			{
-				Console.WriteLine($"Studnet { Name } isnt currently in lesson.");
+				Console.WriteLine($"Student { this.Name } isnt in the lesson, at the moment.");
 			}
 		}
     }
