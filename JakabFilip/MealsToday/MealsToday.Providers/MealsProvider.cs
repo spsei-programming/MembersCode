928bc2159b37ad98d58c2389ab3962bd7f5e9dc5
student <student>
2016-11-09 16:28:52 +0100

    Updated Providers

--- JakabFilip/MealsToday/MealsToday.Providers/MealsProvider.cs
+++ JakabFilip/MealsToday/MealsToday.Providers/MealsProvider.cs
@@ -45,5 +45,39 @@ namespace MealsToday.Providers
 
 			return mealList;
 		}
+
+		/// <summary>
+		/// Adds new Meal to the already existing list
+		/// </summary>
+		/// <param name="actualMealList">an existing list</param>
+		/// <param name="meal">Meal to be added to the existing list</param>
+		/// <returns></returns>
+		public List<Meal> AddMealToDailyMeals(List<Meal> actualMealList, Meal meal)
+		{
+			actualMealList.Add(meal);
+
+			return actualMealList;
+		}
+
+		/// <summary>
+		/// Removes Meal from the existing list 
+		/// </summary>
+		/// <param name="actualMealList">an existing list</param>
+		/// <param name="meal">Meal to be removed from the existing list</param>
+		/// <returns></returns>
+		public List<Meal> RemoveMealFromDailyMeals(List<Meal> actualMealList, Meal meal)
+		{
+			try
+			{
+				actualMealList.Remove(meal);
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("The Following Meal isnt in the List of Daily Meals");
+				return null;
+			}
+
+			return actualMealList;
+		}
 	}
 }
