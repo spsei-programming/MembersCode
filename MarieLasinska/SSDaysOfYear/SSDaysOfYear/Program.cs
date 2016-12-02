fd6271cfa28072a5ccc1c503b7a36a6b503a5131
Arkarianasa <marie.lasinska@seznam.cz>
2016-12-02 17:37:40 +0100

    Homework SSDaysOfYear

---
+++ MarieLasinska/SSDaysOfYear/SSDaysOfYear/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SSDaysOfYear.Helpers;
+
+namespace SSDaysOfYear
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int rok;
+			Console.WriteLine("Zadej rok\n");
+			rok = Convert.ToInt32(Console.ReadLine());
+			SaSuDaysOfYear.CreateYearDirectory(rok);
+		}
+	}
+}
