231cf272a6a9e6b95a6141e2f41b2edcb6a632f1
student <student>
2017-01-09 16:40:46 +0100

    FileManager created

---
+++ MoravecAlbert/FileManager/FileManager/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileManager.Providers;
+
+namespace FileManager
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Providers.FileManager manager = new Providers.FileManager();
+
+			manager.CopyFiles(@"C:\Windows\System32\", "*.png", @"C:\temp", true);
+		}
+	}
+}
---
+++ MoravecAlbert/FileManager/FileManager/Providers/FileManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace FileManager.Providers
+{
+	public class FileManager
+	{
+		public void CopyFile(string from, string to, bool overwrite)
+		{
+			File.Copy(from, to, overwrite);
+		}
+
+		public void RemoveFile(string from)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void CopyFiles(List<string> filesToCopy, string targetDir, bool overwrite)
+		{
+			filesToCopy.ForEach(f => CopyFile(f, Path.Combine(targetDir, /*f.Substring(f.LastIndexOf('\\') + 1))*/, overwrite));
+		}
+
+		public void CopyFiles(string from, string mask, string to, bool overwrite)
+		{
+			Stopwatch watch = Stopwatch.StartNew();
+
+			List<string> files = Directory.GetFiles(from, mask).ToList();
+			
+			CopyFiles(files, to, overwrite);
+
+			watch.Stop();
+			Console.WriteLine(watch.Elapsed);
+		}
+
+		public void RemoveFiles(string from, string mask)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
