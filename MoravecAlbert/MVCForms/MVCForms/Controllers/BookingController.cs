71afe11798fb888e32418c835aa3c27e541e9a94
student <student>
2017-02-01 16:37:46 +0100

    Created model, saved bookings to session

---
+++ MoravecAlbert/MVCForms/MVCForms/Controllers/BookingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCForms.Models.Booking;
+
+namespace MVCForms.Controllers
+{
+	public class BookingController : Controller
+	{
+		public List<BookTableVM> Bookings
+		{
+			get
+			{
+				if (Session["Bookings"] == null)
+				{
+					Session["Bookings"] = new List<BookTableVM>(10);
+				}
+
+				return (List<BookTableVM>) Session["Bookings"];
+			}
+		}
+
+		// GET: Booking
+		public ActionResult Table()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult SaveTable(BookTableVM model)
+		{
+			Bookings.Add(model);
+			return RedirectToAction("Table");
+		}
+	}
+}
---
+++ MoravecAlbert/MVCForms/MVCForms/Models/Booking/BookTableVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MVCForms.Models.Booking
+{
+	public class BookTableVM
+	{
+		public string Name { get; set; }
+
+		public DateTime Date { get; set; }
+
+		public byte AmmountOfPeople { get; set; }
+	}
+}
