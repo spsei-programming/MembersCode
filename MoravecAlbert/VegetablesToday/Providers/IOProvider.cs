dd1086d22f658adc5766655792b6acf8c14c1f4c
student <student>
2017-02-15 16:39:59 +0100

    Created IOProvider

---
+++ MoravecAlbert/VegetablesToday/Providers/IOProvider.cs
@@ -0,0 +1,56 @@
+﻿using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using Data.Base;
+using Newtonsoft.Json;
+
+namespace Providers
+{
+	public class IOProvider
+	{
+		private List<ProductOfNature> stock;
+
+		JsonSerializer jsonSerializer = new JsonSerializer();
+		XmlSerializer xmlSerializer = new XmlSerializer(typeof(ProductOfNature));
+
+		public IOProvider(List<ProductOfNature> stock)
+		{
+			this.stock = stock;
+		}
+
+		public void SaveJSON(string path)
+		{
+			using (StreamWriter streamWriter = new StreamWriter(path))
+			using (JsonWriter jsonWriter = new JsonTextWriter(streamWriter))
+			{
+				jsonSerializer.Serialize(jsonWriter, stock);
+			}
+		}
+
+		public List<ProductOfNature> LoadJson(string path)
+		{
+			using (StreamReader streamReader = new StreamReader(path))
+			using (JsonReader jsonReader = new JsonTextReader(streamReader))
+			{
+				return jsonSerializer.Deserialize(jsonReader) as List<ProductOfNature>;
+			}
+		}
+
+		public ProductOfNature ImportVendorXml(string path)
+		{
+			using (StreamReader streamReader = new StreamReader(path))
+			{
+				return xmlSerializer.Deserialize(streamReader) as ProductOfNature;
+			}
+		}
+
+		public void ExportVendorXml(string path)
+		{
+			using (StreamWriter streamWriter = new StreamWriter(path))
+			{
+				xmlSerializer.Serialize(streamWriter, stock);
+			}
+		}
+	}
+}
